15a73d4bcae7731dc1b8e6514aba0b93b237f525
Elena <bellenaelena@gmail.com>
2022-10-13 18:17:31 +0300

    Добвлено решение задачи 016

--- HomeWork2.py
+++ HomeWork2.py
@@ -26,4 +26,23 @@
 #     prodact *= i
 #     print(prodact, end=' ')
     
-    
+#  16. Задайте список из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
+# Пример:
+# Для n = 6: {1: 2, 2: 2,25, 3: 2,37, 4: 2,44, 5: 2,49, 6: 2,52}
+
+# number = int(input('Введите число: '))
+# my_list = {}
+
+# def MyList(number):
+   
+#     for i in range (1,number+1):
+#         my_list[i] = round((1+1/i)**i, 2)
+#     return my_list  
+
+# print(f'Cписок из {number} чисел последовательности (1+1/{number})^{number} = {MyList(number)} ')    
+
+# my_list2 = [round((1+1/i)**i, 3) for i in range(1, number+1)]
+
+# print(f'Сумма последовательности чисел = {round(sum(my_list2), 2)} ')
+
+
