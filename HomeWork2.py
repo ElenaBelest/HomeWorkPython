1bbf640fef3e85fdebcacb7776eea2b6171f9567
Elena <bellenaelena@gmail.com>
2022-10-13 16:27:08 +0300

    Добвлено решение задачи 015

--- HomeWork2.py
+++ HomeWork2.py
@@ -3,14 +3,27 @@
 # 6782 -> 23
 # 0,56 -> 11
 
-number = input('Enter number: ')
+# number = input('Enter number: ')
 
-def SumNumbers(number):
-    sum = 0
-    for i in number:
-     if i != '.':
-        sum += int(i)
-    return sum
+# def SumNumbers(number):
+#     sum = 0
+#     for i in number:
+#      if i != '.':
+#         sum += int(i)
+#     return sum
 
 
-print(f'The sum of the digits of the number  {number} is {SumNumbers(number)} ')
+# print(f'The sum of the digits of the number  {number} is {SumNumbers(number)} ')
+
+# 15. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+# Пример:
+# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+# number = int(input('Enter number: '))
+# prodact = 1
+
+# for i in range(1,number+1):
+#     prodact *= i
+#     print(prodact, end=' ')
+    
+    
