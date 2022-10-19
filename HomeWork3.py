a20870872229fe54c0045f992c48e847cccd9239
Elena <bellenaelena@gmail.com>
2022-10-19 12:38:40 +0300

    Добвлено решение задачи 002

--- HomeWork3.py
+++ HomeWork3.py
@@ -5,8 +5,25 @@
 
 - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 '''
-my_list = [2, 3, 5, 9, 3]
-sum = 0
-for i in range (1, len(my_list),2):
-     sum += my_list[i]
-print(sum)    
+# my_list = [2, 3, 5, 9, 3]
+# sum = 0
+# for i in range (1, len(my_list),2):
+#      sum += my_list[i]
+# print(sum)    
+
+'''2. Напишите программу, которая найдёт произведение пар чисел списка.
+ Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+*Пример:*
+
+- [2, 3, 4, 5, 6] => [12, 15, 16];
+- [2, 3, 5, 6] => [12, 15]'''
+
+my_list = [2, 3, 4, 5, 6]
+n = len(my_list)
+len = int(len(my_list)/2+1)
+prd = []
+for i in range (len):
+    n = n - 1
+    prd.append (my_list[i]*my_list[n])
+print(prd) 
+   
