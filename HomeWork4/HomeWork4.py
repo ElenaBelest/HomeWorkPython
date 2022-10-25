0cf44e915c1fbcbbdcca67bdd85f8533f1b3df57
Elena <bellenaelena@gmail.com>
2022-10-25 10:45:17 +0300

    Добавлены решения задач 031,032

--- HomeWork4/HomeWork4.py
+++ HomeWork4/HomeWork4.py
@@ -3,29 +3,63 @@
 Написать код, загружающий данные из обоих файлов и формирующий из них словарь: 
 ключи — ФИО, значения — данные о хобби.'''
 
-users1 = []
-hobby1 = []
-with open('D:/Lena/Geekbrains/HomeWork/HWPython/HomeWork4/users.txt', 'r', encoding='utf-8') as users:
-      for line in users:
-        users1.append(line)
-str1 = users1
+# users1 = []
+# hobby1 = []
+# with open('D:/Lena/Geekbrains/HomeWork/HWPython/HomeWork4/users.txt', 'r', encoding='utf-8') as users:
+#       for line in users:
+#         users1.append(line)
+# str1 = users1
    
-with open('D:/Lena/Geekbrains/HomeWork/HWPython/HomeWork4/hobby.txt', 'r', encoding='utf-8') as hobby:
-      for line1 in hobby:
-        hobby1.append(line1)
-str2 = hobby1      
-# print(str1,str2)
-info = dict(zip(str1,str2))
-# print(info)
-
+# with open('D:/Lena/Geekbrains/HomeWork/HWPython/HomeWork4/hobby.txt', 'r', encoding='utf-8') as hobby:
+#       for line1 in hobby:
+#         hobby1.append(line1)
+# str2 = hobby1      
+# # print(str1,str2)
+# info = dict(zip(str1,str2))
+# # print(info)
 # with open('D:/Lena/Geekbrains/HomeWork/HWPython/HomeWork4/users_hobby.txt', 'w',encoding='utf-8') as out:
-#     out.writelines(info)
-
-
-with open('D:/Lena/Geekbrains/HomeWork/HWPython/HomeWork4/users_hobby.txt', 'w',encoding='utf-8') as out:
-    for key, val in info.items():
-        out.write('{}:{}\n'.format(key, val))
-'''Проверка видимости файлов'''
+#     for key, val in info.items():
+#         out.write('{}:{}\n'.format(key, val))
+'''Проверка видимости файлов.У меня возникла проблема, что файлы txt не виделись python, поэтому пришлось прописывать весь путь'''
 # import os
 # # print (os.getcwd())
 # print (os.listdir(os.getcwd()))
+
+'''31. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.'''
+
+# number = int(input('Введите натуральное число: '))
+
+# def Multipliers(number):
+#     multipliers = []
+#     item = 2
+#     while (item <= number):
+#         if number % item == 0:
+#             multipliers.append(item)
+#             number = number/item
+#         else:
+#             item +=1    
+#     return multipliers   
+
+# print(f'Множители числа {number} = {Multipliers(number)} ')
+
+'''32. Задайте последовательность чисел. Напишите программу,
+ которая выведет список неповторяющихся элементов исходной последовательности.'''
+
+# my_lists = ['1','5','2','1','45','8','2','45']
+# print(f'Заданный список: {my_lists}')
+# my_lists1 = []
+# for i in my_lists:
+#     if i not in my_lists1:
+#         my_lists1.append(i)
+# print(f'Список не повторяющихся элементов: {my_lists1}')    
+   
+# my_lists = list(map(int, input("Введите числа через пробел:\n").split()))
+# print(f"Заданный список: {my_lists}")
+# new_lists = []
+# [new_lists.append(i) for i in my_lists if i not in new_lists]
+# print(f"Список из неповторяющихся элементов: {new_lists}")
+
+'''33. Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
+(значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+Пример:
+k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10x² = 0'''
